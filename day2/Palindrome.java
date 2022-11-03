9ba3f1b141da35fb8af955b0dc60707e8f42786a
Anju Sreekumar <93622823+anjtoanj@users.noreply.github.com>
2022-11-04 01:14:34 +1300

    Palindrome

---
+++ day2/Palindrome.java
@@ -0,0 +1,32 @@
+package week1.day2;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		//Build a logic to find the given string is palindrome or not
+	     
+		/*
+		 * Pseudo Code
+		
+		 * a) Declare A String value as"madam"
+		 
+		 * b) Declare another String rev value as ""
+		 * c) Iterate over the String in reverse order
+		 * d) Add the char into rev
+		 * e) Compare the original String with the reversed String, if it is same then print palinDrome 
+		 
+		 * Hint: Use .equals or .equalsIgnoreCase when you compare a String 
+		 */
+		String svalue = "madam";
+		String rev ="";
+		
+		char[] str =svalue.toCharArray();
+		for(int i=svalue.length()-1; i>=0;i--) {
+		  //  System.out.println(str[1]);
+			rev= rev+str[i];
+		//	System.out.println(rev);
+		}
+		if (svalue.equals(rev)) {System.out.println("The string: " +svalue+" is palindrome");}
+	}
+
+}
